11c810ccdf91c755a153e45886d556f47eb5dc6e
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-29 17:06:45 +0300

    fix some image issues

--- script/carousel.js
+++ script/carousel.js
@@ -1,11 +1,28 @@
-window.addEventListener('scroll', () => {
+window.addEventListener('load', () => {
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
         loop: true,
         centeredSlides: true,
-        autoplay: {
+        // Do not start autoplay initially
+        autoplay: false,
+    });
+
+    // Function to enable autoplay when user interacts with the page
+    const startAutoplay = () => {
+        swiper.params.autoplay = {
             delay: 2500,
             disableOnInteraction: false,
-        },
+        };
+        swiper.autoplay.start();
+
+        // Remove event listeners after autoplay has started
+        ['click', 'mousemove', 'keypress', 'scroll'].forEach((event) => {
+            document.removeEventListener(event, startAutoplay);
+        });
+    };
+
+    // Add event listeners for user interactions
+    ['click', 'mousemove', 'keypress', 'scroll'].forEach((event) => {
+        document.addEventListener(event, startAutoplay, { once: true });
     });
-}, { once: true });
+});

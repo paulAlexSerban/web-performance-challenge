be1cde9114759dbd1b70a7a43782ec56f106e544
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-29 14:16:33 +0300

    fix: missing lazuload in console.lg

--- script/carousel.js
+++ script/carousel.js
@@ -1,9 +1,11 @@
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+window.addEventListener('load', () => {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
 });
